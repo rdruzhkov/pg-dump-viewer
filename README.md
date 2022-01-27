5b51f8b7675c1400e5005c55e0150d1780fa2281
druzhkovri <r.druzhov99@gmail.com>
2022-01-27 15:58:07 +0300

    Implemented column info parsing.

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-
-# TODO:
-# 1. [x] Learn how to read tables list
-# 2. [ ] Learn how to read column list
-# 3. [x] Learn how to read table data
+TODO:
+1. [x] Learn how to read tables list
+2. [x] Learn how to read column list
+3. [x] Learn how to read table data
+4. [ ] Implement GUI
+5. [ ] Fill README
--- dump.py
+++ dump.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from typing import List
 
 import pgdumplib
+import sqlparse
 
 
 class Dump:
@@ -14,8 +15,43 @@ class Dump:
             if entry.desc == 'TABLE':
                 self.__tables.append({
                     'name': entry.tag,
-                    'namespace': entry.namespace
+                    'namespace': entry.namespace,
+                    'columns': Dump.__get_columns_info(entry.defn)
                 })
+
+    @staticmethod
+    def __get_columns_info(table_create_statement: str):
+        columns_info = []
+        parse = sqlparse.parse(table_create_statement)
+        for stmt in parse:
+            # Get all the tokens except whitespaces
+            tokens = [t for t in sqlparse.sql.TokenList(stmt.tokens) if t.ttype != sqlparse.tokens.Whitespace]
+            is_create_stmt = False
+            for i, token in enumerate(tokens):
+                # Is it a create statements ?
+                if token.match(sqlparse.tokens.DDL, 'CREATE'):
+                    is_create_stmt = True
+                    continue
+
+                # If it was a create statement and the current token starts with "("
+                if is_create_stmt and token.value.startswith("("):
+                    # Get the table name by looking at the tokens in reverse order till you find
+                    # a token with None type
+
+                    # Now parse the columns
+                    txt = token.value
+                    columns = txt[1:txt.rfind(")")].replace("\n", "").split(",")
+                    for column in columns:
+                        c = ' '.join(column.split()).split()
+                        c_name = c[0].replace('\"', "")
+                        c_type = c[1]  # For condensed type information
+                        # OR
+                        # c_type = " ".join(c[1:]) # For detailed type information
+                        columns_info.append({'name': c_name, 'data_type': c_type})
+
+                    break
+
+        return columns_info
 
     @property
     def database_name(self) -> str:
@@ -33,9 +69,15 @@ class Dump:
     def dump_version(self) -> str:
         return self.__dump.dump_version
 
-    def get_tables(self) -> List[str]:
+    def get_tables(self) -> List[dict]:
         return self.__tables.copy()
 
+    def get_table_columns(self, table: str, namespace: str):
+        pass
 
-# for line in dump.table_data('public', 'test_table_1'):
-#     print(line)
+    def get_table_data(self, table: str, namespace: str):
+        table_data = []
+        for line in self.__dump.table_data(namespace, table):
+            table_data.append(line)
+
+        return table_data
